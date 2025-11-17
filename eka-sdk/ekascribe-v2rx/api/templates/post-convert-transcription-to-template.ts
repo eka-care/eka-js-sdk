54de86d00a39b849d8737653df393ba12411bac0
Sanikagoyal <sanikagoyal9@gmail.com>
2025-11-17 20:50:58 +0530

    updated api path

--- eka-sdk/ekascribe-v2rx/api/templates/post-convert-transcription-to-template.ts
+++ eka-sdk/ekascribe-v2rx/api/templates/post-convert-transcription-to-template.ts
@@ -25,7 +25,7 @@ async function postConvertTranscriptionToTemplate({
     };
 
     const response = await fetchWrapper(
-      `${GET_EKA_VOICE_HOST_V1()}/transaction/${txn_id}/convert-to-template`,
+      `${GET_EKA_VOICE_HOST_V1()}/transaction/${txn_id}/convert-to-template?transcript=true`,
       options,
       60000
     );
