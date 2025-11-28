6f60d9caac3f98de7a95a143526878a86ee2dbd0
Sanikagoyal <sanikagoyal9@gmail.com>
2025-11-28 16:27:01 +0530

    added status api to fetch transcription

---
+++ eka-sdk/ekascribe-v2rx/api/transaction/get-voice-api-v3-status-transcription.ts
@@ -0,0 +1,101 @@
+import { SDK_STATUS_CODE } from '../../constants/constant';
+import fetchWrapper from '../../fetch-client';
+import { GET_EKA_VOICE_HOST_V3 } from '../../fetch-client/helper';
+
+export type TTemplateMessage = {
+  type: 'warning' | 'error';
+  code?: string;
+  msg: string;
+};
+
+export type TOutputSummary = {
+  template_id: string;
+  value?: string | null; //<base 64 encoded>
+  type: string;
+  name: string;
+  lang?: string;
+  status: TTemplateStatus;
+  errors?: TTemplateMessage[];
+  warnings?: TTemplateMessage[];
+};
+
+export type TTemplateStatus = 'success' | 'partial_success' | 'failure';
+
+type TAdditionalData = {
+  doctor: {
+    _id: string;
+    profile: {
+      personal: {
+        name: {
+          l: string;
+          f: string;
+        };
+      };
+    };
+  };
+};
+
+type TApiResponse = {
+  data: {
+    output: TOutputSummary[];
+    audio_matrix?: {
+      quality: string;
+    };
+    additional_data?: TAdditionalData;
+    meta_data?: {
+      total_resources?: number;
+      total_parsed_resources?: number;
+    };
+    created_at?: string;
+    template_results: {
+      integration: TOutputSummary[];
+      custom: TOutputSummary[];
+      transcript: TOutputSummary[];
+    };
+  };
+  error?: {
+    code: string;
+    msg: string;
+  };
+};
+
+export type TGetStatusResponse = {
+  response?: TApiResponse | null;
+  status_code: number;
+  message?: string;
+};
+
+export const getVoiceApiV3StatusTranscription = async ({
+  txnId,
+}: {
+  txnId: string;
+}): Promise<TGetStatusResponse> => {
+  try {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    const options = {
+      method: 'GET',
+      headers,
+    };
+
+    // Use custom timeout for this API (16 seconds instead of default 5 seconds)
+    const getResponse = await fetchWrapper(
+      `${GET_EKA_VOICE_HOST_V3()}/status/transcription/${txnId}`,
+      options,
+      16000
+    );
+
+    const response = await getResponse.json();
+
+    return {
+      response,
+      status_code: getResponse.status,
+    };
+  } catch (error) {
+    return {
+      status_code: SDK_STATUS_CODE.INTERNAL_SERVER_ERROR,
+      message: `Something went wrong! ${error}`,
+    };
+  }
+};
