8c0cacc8a34d0f50f49dc82ea6454f9534356c96
Sanikagoyal <sanikagoyal9@gmail.com>
2025-12-08 13:42:15 +0530

    fixed s3 upload file names

--- eka-sdk/ekascribe-v2rx/shared-worker/s3-file-upload.ts
+++ eka-sdk/ekascribe-v2rx/shared-worker/s3-file-upload.ts
@@ -4,7 +4,7 @@ if (typeof window === 'undefined') {
 }
 
 import { configureAWS } from '../aws-services/configure-aws';
-import pushFileToS3 from '../aws-services/upload-file-to-s3';
+import pushFileToS3 from '../aws-services/upload-file-to-s3-es11';
 import { AUDIO_EXTENSION_TYPE_MAP, OUTPUT_FORMAT } from '../constants/constant';
 import { SHARED_WORKER_ACTION } from '../constants/enums';
 import compressAudioToMp3 from '../utils/compress-mp3-audio';
