d77f7868f4b24438399fa85aa30a55aaad2cf3f0
Sanikagoyal <sanikagoyal9@gmail.com>
2026-02-17 17:05:57 +0530

    updated build process for sharedworker in es6

--- packages/es6/build.mjs
+++ packages/es6/build.mjs
@@ -6,7 +6,9 @@ const sharedConfig = {
   platform: 'browser',
   target: ['es6'],
   sourcemap: true,
-  minify: false,
+  minify: true,
+  treeShaking: true,
+  drop: ['console', 'debugger'],
   define: {
     'global': 'globalThis',
     'process.env.NODE_ENV': '"production"',
@@ -34,6 +36,33 @@ await esbuild.build({
   ...sharedConfig,
   outfile: 'dist/index.esm.js',
   format: 'esm',
+});
+
+// SharedWorker build (IIFE format, matching ES11 worker bundle)
+await esbuild.build({
+  entryPoints: ['../../eka-sdk/ekascribe-v2rx/shared-worker/s3-file-upload.ts'],
+  bundle: true,
+  platform: 'browser',
+  target: ['es6'],
+  minify: true,
+  treeShaking: true,
+  drop: ['console', 'debugger'],
+  format: 'iife',
+  globalName: 'EkaS3Worker',
+  outfile: 'dist/worker.bundle.js',
+  define: {
+    'global': 'globalThis',
+    'process.env.NODE_ENV': '"production"',
+    'process.env.NODE_DEBUG': 'false',
+    'process': '{"env": {}}',
+  },
+  banner: {
+    js: `
+      if (typeof globalThis === 'undefined') {
+        var globalThis = typeof window !== 'undefined' ? window : typeof self !== 'undefined' ? self : {};
+      }
+    `,
+  },
 });
 
 console.log('Build complete!');
